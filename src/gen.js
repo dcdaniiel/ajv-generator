92a85c09c6cbc6d63eee35bd8c39e4e9f8f99e7c
Daniel Teixeira <dc.daniiel@gmail.com>
2022-03-10 19:36:45 -0300

    add type array

--- src/gen.js
+++ src/gen.js
@@ -1,13 +1,22 @@
+const Ajv = require('ajv')
+const AjvKeywords = require('ajv-keywords')
+
 const { getType } = require('./types')
 
-const schema = {
+const data = {
   code: 1,
   data: [
     {
-      lote: 2430,
-      status: '1',
-      sub_of: false,
-      em_fila: true,
+      detalhes: [
+        {
+          motivo: 'Sem Match de palavras',
+          status: 'aprovado',
+          id_captura: 1,
+          id_pesquisa_consulta: 21401,
+        },
+      ],
+      id_dossie: 21401,
+      workflowStatus: 'aprovado',
     },
   ],
   status: 200,
@@ -19,16 +28,11 @@ const genSchema = (obj) => {
     Object.entries(schema).reduce((acc, [key, val]) => {
       const type = getType(val)
 
-      if (type === 'array') {
-        return {
-          type,
-          items: genSchema(val[0]),
-        }
-      }
+      const items = type === 'array' ? { items: genSchema(val[0]) } : {}
 
       return {
         ...acc,
-        [key]: { type: getType(val) },
+        [key]: { type: getType(val), ...items },
       }
     }, {})
 
@@ -41,14 +45,23 @@ const genSchema = (obj) => {
   }
 }
 
-const result = genSchema(schema)
+const schema = genSchema(data)
 
-console.log(JSON.stringify(result, null, 2))
+console.log('SCHEMA ::: ', JSON.stringify(schema, null, 2))
 
-// {"code": 1, "data": [{"detalhes": [{"motivo": "Sem Match de palavras", "status": "aprovado", "id_captura": 1, "id_pesquisa_consulta": 21401}], "id_dossie": 21401, "workflowStatus": "aprovado"}], "status": 200, "message": "OK"}
-//
-// {"code": 1, "data": [{"detalhes": [{"motivo": "Nao atendeu nenhuma das regras criadas no workflow", "status": "aprovado", "id_captura": 173, "id_pesquisa_consulta": 21705}], "id_dossie": 21705, "workflowStatus": "aprovado"}], "status": 200, "message": "OK"}
-//
-// {"data": {"compositeResponse": [{"body": {"id": "a055e000009iuAdAAI", "errors": [], "created": false, "success": true}, "httpHeaders": {"Location": "/services/data/v52.0/sobjects/PedidoEmprestimo__c/a055e000009iuAdAAI"}, "referenceId": "approvalId_12431767", "httpStatusCode": 200}, {"body": {"id": "0015e00000o8DC0AAM", "errors": [], "created": false, "success": true}, "httpHeaders": {"Location": "/services/data/v52.0/sobjects/Account/0015e00000o8DC0AAM"}, "referenceId": "refConta", "httpStatusCode": 200}]}, "status": 200}
-//
-// {"data": {"compositeResponse" : [{"body": [{"fields": ["Email__c"], "message": "Campos obrigatórios ausentes: [Email__c]", "errorCode": "REQUIRED_FIELD_MISSING"}], "httpHeaders": {}, "referenceId": "refConta", "httpStatusCode": 400}]}, "status": 200}
+const ajv = new Ajv()
+AjvKeywords(ajv)
+
+const validate = ajv.compile(schema)
+
+const valid = validate(data)
+
+console.log('VALIDATE ::: ', { valid })
+
+/*
+    {"code": 1, "data": [{"lote": 2430, "status": "1", "sub_of": false, "em_fila": true}], "status": 200, "message": "OK"}
+    {"code": 1, "data": [{"detalhes": [{"motivo": "Sem Match de palavras", "status": "aprovado", "id_captura": 1, "id_pesquisa_consulta": 21401}], "id_dossie": 21401, "workflowStatus": "aprovado"}], "status": 200, "message": "OK"}
+    {"code": 1, "data": [{"detalhes": [{"motivo": "Nao atendeu nenhuma das regras criadas no workflow", "status": "aprovado", "id_captura": 173, "id_pesquisa_consulta": 21705}], "id_dossie": 21705, "workflowStatus": "aprovado"}], "status": 200, "message": "OK"}
+    {"data": {"compositeResponse": [{"body": {"id": "a055e000009iuAdAAI", "errors": [], "created": false, "success": true}, "httpHeaders": {"Location": "/services/data/v52.0/sobjects/PedidoEmprestimo__c/a055e000009iuAdAAI"}, "referenceId": "approvalId_12431767", "httpStatusCode": 200}, {"body": {"id": "0015e00000o8DC0AAM", "errors": [], "created": false, "success": true}, "httpHeaders": {"Location": "/services/data/v52.0/sobjects/Account/0015e00000o8DC0AAM"}, "referenceId": "refConta", "httpStatusCode": 200}]}, "status": 200}
+    {"data": {"compositeResponse": [{"body": [{"fields": ["Email__c"], "message": "Campos obrigatórios ausentes: [Email__c]", "errorCode": "REQUIRED_FIELD_MISSING"}], "httpHeaders": {}, "referenceId": "refConta", "httpStatusCode": 400}]}, "status": 200}
+ */
